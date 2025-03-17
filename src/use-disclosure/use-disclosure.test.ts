588ec8bc83f56871c7858268c3b02fa57a90fd0d
Mikael Balin <hello@mikaelbalin.com>
2025-03-17 15:57:09 +0000

    test: enhance useDisclosure tests with onOpen and onClose callbacks

--- src/use-disclosure/use-disclosure.test.ts
+++ src/use-disclosure/use-disclosure.test.ts
@@ -1,7 +1,7 @@
 import { act, renderHook } from "@testing-library/react";
 import { useDisclosure } from "./use-disclosure";
 
-describe("use-disclosure", () => {
+describe("useDisclosure", () => {
   it("handles close correctly", () => {
     const hook = renderHook(() => useDisclosure(true));
     expect(hook.result.current[0]).toBe(true);
@@ -27,5 +27,49 @@ describe("use-disclosure", () => {
 
     act(() => hook.result.current[1].toggle());
     expect(hook.result.current[0]).toBe(false);
+  });
+
+  it("calls onClose when close is called", () => {
+    const spy = vi.fn();
+    const hook = renderHook(() => useDisclosure(true, { onClose: spy }));
+    expect(spy).toHaveBeenCalledTimes(0);
+
+    act(() => hook.result.current[1].close());
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    act(() => hook.result.current[1].close());
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls onOpen when open is called", () => {
+    const spy = vi.fn();
+    const hook = renderHook(() => useDisclosure(false, { onOpen: spy }));
+    expect(spy).toHaveBeenCalledTimes(0);
+
+    act(() => hook.result.current[1].open());
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    act(() => hook.result.current[1].open());
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls onOpen and onClose correctly when toggle is called", () => {
+    const onClose = vi.fn();
+    const onOpen = vi.fn();
+    const hook = renderHook(() => useDisclosure(false, { onOpen, onClose }));
+    expect(onOpen).toHaveBeenCalledTimes(0);
+    expect(onClose).toHaveBeenCalledTimes(0);
+
+    act(() => hook.result.current[1].toggle());
+    expect(onOpen).toHaveBeenCalledTimes(1);
+    expect(onClose).toHaveBeenCalledTimes(0);
+
+    act(() => hook.result.current[1].toggle());
+    expect(onOpen).toHaveBeenCalledTimes(1);
+    expect(onClose).toHaveBeenCalledTimes(1);
+
+    act(() => hook.result.current[1].toggle());
+    expect(onOpen).toHaveBeenCalledTimes(2);
+    expect(onClose).toHaveBeenCalledTimes(1);
   });
 });
